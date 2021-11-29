52a2b58cb50ffa9f51d3407792624f104bcb1d28
Maicon Zucco <maicon.zucco@me.com>
2021-11-29 22:54:43 +0100

    Change APIURL to translate.googleapis.com.

--- src/folder/store.ts
+++ src/folder/store.ts
@@ -133,6 +133,7 @@ export default class FolderModule extends VuexModule {
     const { googleTranslateApiKey } = this.context.rootState.settings.settings;
 
     let supportedLanguages: string[] = [];
+    /*
     if (googleTranslateApiKey) {
       try {
         const supportedLanguagesResponse = await fetch(
@@ -147,7 +148,11 @@ export default class FolderModule extends VuexModule {
       } catch (e) {
         this.context.commit('setTranslationEnabled', false);
       }
-    }
+    }*/
+    // https://cloud.google.com/translate/docs/languages
+    supportedLanguages = ['af', 'sq', 'am', 'ar', 'hy', 'az', 'eu', 'be', 'bn', 'bs', 'bg', 'ca', 'ceb', 'zh-CN', 'zh-TW', 'co', 'hr', 'cs', 'da', 'nl', 'en', 'eo', 'et', 'fi', 'fr', 'fy', 'gl', 'ka', 'de', 'el', 'gu', 'ht', 'ha', 'haw', 'he', 'hi', 'hmn', 'hu', 'is', 'ig', 'id', 'ga', 'it', 'ja', 'jv', 'kn', 'kk', 'km', 'rw', 'ko', 'ku', 'ky', 'lo', 'lv', 'lt', 'lb', 'mk', 'mg', 'ms', 'ml', 'mt', 'mi', 'mr', 'mn', 'my', 'ne', 'no', 'ny', 'or', 'ps', 'fa', 'pl', 'pt', 'pa', 'ro', 'ru', 'sm', 'gd', 'sr', 'st', 'sn', 'sd', 'si', 'sk', 'sl', 'so', 'es', 'su', 'sw', 'sv', 'tl', 'tg', 'ta', 'tt', 'te', 'th', 'tr', 'tk', 'uk', 'ur', 'ug', 'uz', 'vi', 'cy', 'xh', 'yi', 'yo', 'zu'];
+
+    this.context.commit('setTranslationEnabled', true);
 
     const languageList = createLanguageList(this.tree, this.folder, supportedLanguages);
 
